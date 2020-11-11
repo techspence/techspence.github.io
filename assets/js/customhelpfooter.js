068016e803aecd145a3c80d6c5cb8f6085150dd8
Spencer Alessi <alessisb@gmail.com>
2020-11-11 08:29:23 -0500

    Update customhelpfooter.js

--- assets/js/customhelpfooter.js
+++ assets/js/customhelpfooter.js
@@ -1,5 +1,5 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM is fully loaded');
+window.addEventListener('load', (event) => {
+    console.log('page is fully loaded');
     console.log(document.getElementById("help-links-container").style.display);
     document.getElementById("help-links-container").style.display = "block";
     console.log(document.getElementById("help-links-container").style.display);
