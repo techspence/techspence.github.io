a6146fdef36c07c87b7dbe256ab5a4de935295e6
Spencer Alessi <alessisb@gmail.com>
2020-11-10 20:59:51 -0500

    Update customhelpfooter.js

--- assets/js/customhelpfooter.js
+++ assets/js/customhelpfooter.js
@@ -1,3 +1,6 @@
-window.onload=function() {
+window.addEventListener('load', (event) => {
+    console.log('page is fully loaded');
+    console.log(document.getElementById("help-links-container").style.display);
     document.getElementById("help-links-container").style.display = "block";
-};
+    console.log(document.getElementById("help-links-container").style.display);
+});
