05bf21f87c49a49c48e570bea78736c1fc7a97c3
Spencer Alessi <alessisb@gmail.com>
2020-11-19 08:35:07 -0500

    Delete customhelpfooter.js

--- assets/js/customhelpfooter.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', (event) => {
-    console.log('page is fully loaded');
-    console.log(document.getElementById("help-links-container").style.display);
-    document.getElementById("help-links-container").style.display = "block";
-    console.log(document.getElementById("help-links-container").style.display);
-});
